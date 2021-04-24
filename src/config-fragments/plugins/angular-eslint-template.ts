7f3a7ba167512def20a4cd9a0992647bad403080
GerkinDev <alexandre.j.p.germain@gmail.com>
2021-04-24 15:20:18 +0200

    feat(angular): update to latest plugins, use more strict rules

--- src/config-fragments/plugins/angular-eslint-template.ts
+++ src/config-fragments/plugins/angular-eslint-template.ts
@@ -4,10 +4,14 @@ export = {
 		'@angular-eslint/template',
 	],
 	extends: [
-		'plugin:@angular-eslint/template/recommended',
+		'plugin:@angular-eslint/template/all',
 	],
 	rules: {
 		'@angular-eslint/template/cyclomatic-complexity': 'warn',
 		'@angular-eslint/template/no-call-expression': 'warn',
+		'@angular-eslint/template/click-events-have-key-events': 'warn',
+		'@angular-eslint/template/i18n': 'warn',
+		'@angular-eslint/template/no-any': 'warn',
+		'@angular-eslint/template/use-track-by-function': 'off',
 	},
 };
--- src/config-fragments/plugins/angular-eslint.ts
+++ src/config-fragments/plugins/angular-eslint.ts
@@ -2,10 +2,5 @@ export = {
 	plugins: [
 		'@angular-eslint',
 	],
-	extends: 'plugin:@angular-eslint/recommended',
-	rules: {
-		'@angular-eslint/prefer-output-readonly': 'error',
-		// '@angular-eslint/prefer-input-readonly': 'error',
-		// '@angular-eslint/prefer-inline-decorator': 'warn',
-	},
+	extends: 'plugin:@angular-eslint/all',
 }
