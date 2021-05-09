63e544a074b071b60fc668839c98aa4b65c97df2
GerkinDev <alexandre.j.p.germain@gmail.com>
2021-05-09 15:11:41 +0200

    feat(angular): customize default rules

--- src/config-fragments/plugins/angular-eslint.ts
+++ src/config-fragments/plugins/angular-eslint.ts
@@ -3,4 +3,8 @@ export = {
 		'@angular-eslint',
 	],
 	extends: 'plugin:@angular-eslint/all',
+	rules: {
+		'@angular-eslint/use-component-selector': 'off',
+		'@angular-eslint/no-forward-ref': 'warn',
+	},
 }
